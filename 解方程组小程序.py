c35e8a1b7558e19960b99434d3320c81722c0f26
thunder-sword <64960284+thunder-sword@users.noreply.github.com>
2021-01-09 13:46:28 +0800

    解方程组小程序v2.0
    添加了自动分析变量的代码，现在输入只需要输入方程组便可以得到结果

--- 解方程组小程序.py
+++ 解方程组小程序.py
@@ -1,23 +1,40 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from sympy import solve,Symbol 
-#解方程组小程序v1.0-thunder-sword
-#处理了输出内容，使其整齐化
-#如4*x可以直接写为4x，能识别
-#可以用^或者**进行指数运算操作
+#解方程组小程序v2.0-thunder-sword
+#添加了自动分析变量的代码，现在输入只需要输入方程组便可以得到结果
 
-print('请输入方程组中所有的变量名：')
+
 bianliang=[]
-while true:
-    string=input()
-    if string=='':
-        break
-    bianliang.append(string)
-    exec("{0}=Symbol('{0}')".format(string))
 print('请输入方程组中所有的方程：')
 fangcheng=[]
-while true:
+while True:
     string2=input()
+    if string2=='':
+        break
+    #自动读入分析变量名
+    temp_bianliang=''
+    flag_1 = False
+    for i in range(len(string2)):
+        if flag_1==False and string2[i].isalpha():
+            temp_bianliang+=string2[i]
+            flag_1=True
+        elif flag_1:
+            if string2[i].isalnum():
+                temp_bianliang += string2[i]
+            else:
+                if temp_bianliang not in bianliang:
+                    bianliang.append(temp_bianliang)
+                temp_bianliang=''
+                flag_1=False
+    if flag_1:
+        if temp_bianliang not in bianliang:
+            bianliang.append(temp_bianliang)
+        temp_bianliang = ''
+        flag_1 = False
+    #将bianliang元组中的所有变量初始化
+    for eachbianliang in bianliang:
+        exec("{0}=Symbol('{0}')".format(eachbianliang))
     #处理等于号，使第一个等号变为减号，等号右边被括号括住被减
     ind = string2.find('=')
     if ind!=-1:
@@ -31,9 +48,6 @@ while true:
             ind2=string2.find(i.strip(),ind2+1)
     #将^号切换为**，以便指数运算
     string2=string2.replace('^','**')
-    #print(string2)
-    if string2=='':
-        break
     fangcheng.append(eval(string2))
 
 ans_solve=solve(fangcheng, bianliang)
